7812e7346c407802e244a35cd2dc213ca11047b6
mirumi <info@milmemo.net>
2022-03-19 22:53:00 +0900

    [remove] : vue-toaster

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
-// eslint-disable-next-line
-// @ts-nocheck@
-
 import App from "./App.vue"
 import axios from "axios"
 import VueAxios from "vue-axios"
 import router from "./router/router"
-import Toaster from "@/components/modules/vue-toaster"
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 
-const app = createApp(App)
+createApp(App)
   .use(router)
   .use(createPinia())
   .use(VueAxios, axios)
   .mount("#app")
-
-// app.use(Toaster, {
-//   position: "bottom-right",
-//   duration: 5555,
-// }).provide("toast", app.config.globalProperties.$toast)
-  
-// app.mount("#app")
