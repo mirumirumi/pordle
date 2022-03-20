aaa843fa2f0d225f6fe68b01206647e9aabe1691
mirumi <info@milmemo.net>
2022-03-20 17:18:42 +0900

    [fix] : ignore `status` prop when compare with answer

--- src/lib/compare-with-answer.ts
+++ src/lib/compare-with-answer.ts
@@ -1,5 +1,4 @@
 import { Card, Cards, Status } from "./defines"
-import { isSameObjects } from "./utils"
 
 export default (cards_A: Cards<Card>, cards_B: Cards<Card>): Array<Status> => {
   const result: Array<Status> = Array(5).fill("failure")
@@ -11,7 +10,7 @@ export default (cards_A: Cards<Card>, cards_B: Cards<Card>): Array<Status> => {
     }
     
     for (const card of cards_B) {
-      if (isSameObjects(cards_A[i] as Record<string, unknown>, card as Record<string, unknown>)) {
+      if (cards_A[i].suit === card.suit && cards_A[i].number === card.number) {
         result[i] = "blow"
         continue
       }
