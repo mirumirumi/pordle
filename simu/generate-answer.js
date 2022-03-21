0e0cf23f4b8479ebdbd184a97ecdccbf2977ef69
mirumi <info@milmemo.net>
2022-03-21 21:17:03 +0900

    [add] : royal-straight-flush simulation

---
+++ simu/generate-answer.js
@@ -0,0 +1,388 @@
+const seedrandom = require("seedrandom")
+const validateHand = require("validate-hand")
+
+function isSameObjects(_a, _b) {
+  const _aJSON = JSON.stringify(Object.entries(_a).sort())
+  const _bJSON = JSON.stringify(Object.entries(_b).sort())
+  return _aJSON === _bJSON
+}
+
+module.exports = (seed) => {
+  seedrandom(seed, { global: true })
+
+  const generatedCards = [{}, {}, {}, {}, {}]
+  let isAlreadyExistSameCard = false
+  
+  do {
+    const suits = []
+    const nums = []
+    isAlreadyExistSameCard = false
+
+    for (let i = 0; i < 5; i++) {
+      suits.push(randomInt(3))
+      nums.push(randomInt(12))
+    }
+
+    for (let i = 0; i < 5; i++) {
+      let suit = ""
+      if (suits[i] === 0)
+        suit = "spade"
+      if (suits[i] === 1)
+        suit = "heart"
+      if (suits[i] === 2)
+        suit = "diamond"
+      if (suits[i] === 3)
+        suit = "club"
+
+      for (const card of generatedCards) {
+        if (isSameObjects({ suit: suit, number: nums[i] + 1 }, card)) {
+          isAlreadyExistSameCard = true
+          continue
+        }
+      }
+  
+      generatedCards[i].suit = suit
+      generatedCards[i].number = nums[i] + 1
+    }
+  } while (!validateHand(generatedCards) || isAlreadyExistSameCard)
+
+  // sort
+  const hand = validateHand(generatedCards)
+  const result = [{}, {}, {}, {}, {}]
+  switch (hand) {
+    case "2-pair": {
+      let index = 0
+      const indexes = []
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            if (generatedCards.filter(e => e.number === sorted.number).length === 2) {
+              result[index] = generatedCards[i]
+              indexes.push(i)
+              index++
+            }
+          }
+        }
+      }
+      for (let i = 0; i < 5; i++) {
+        if (!indexes.includes(i)) {
+          result[4] = generatedCards[i]
+        }
+      }
+      break
+    }
+    case "3-card": {
+      let index = 0
+      const indexes = []
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            if (generatedCards.filter(e => e.number === sorted.number).length === 3) {
+              result[index] = generatedCards[i]
+              indexes.push(i)
+              index++
+            }
+          }
+        }
+      }
+      let index2 = 3
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (!indexes.includes(i) && sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            result[index2] = generatedCards[i]
+            index2++
+          }
+        }
+      }
+      break
+    }
+    case "full-house": {
+      let index = 3
+      let index2 = 0
+      const indexes = []
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            if (generatedCards.filter(e => e.number === sorted.number).length === 2) {
+              result[index] = generatedCards[i]
+              indexes.push(i)
+              index++
+            } else if (generatedCards.filter(e => e.number === sorted.number).length === 3) {
+              result[index2] = generatedCards[i]
+              indexes.push(i)
+              index2++
+            }
+          }
+        }
+      }
+      break
+    }
+    case "4-card": {
+      let index = 0
+      const indexes = []
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            if (generatedCards.filter(e => e.number === sorted.number).length === 4) {
+              result[index] = generatedCards[i]
+              indexes.push(i)
+              index++
+            }
+          }
+        }
+      }
+      for (let i = 0; i < 5; i++) {
+        if (!indexes.includes(i)) {
+          result[4] = generatedCards[i]
+        }
+      }
+      break
+    }
+    case "straight": {
+      let index = 0
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            result[index] = generatedCards[i]
+            index++
+          }
+        }
+      }
+      if (result[0].number === 1 && result[1].number === 5 && result[2].number === 4 && result[3].number === 3 && result[4].number === 2) {  // for 5-High-Straight
+        const _ = result[0]
+        result.splice(0, 1)
+        result.push(_)
+      }
+      break
+    }
+    case "flush": {
+      let index = 0
+      for (const sorted of sortedCards) {
+        for (let i = 0; i < 5; i++) {
+          if (sorted.suit === generatedCards[i].suit && sorted.number === generatedCards[i].number) {
+            result[index] = generatedCards[i]
+            index++
+          }
+        }
+      }
+      break
+    }
+  }
+
+  return result
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+const sortedCards = [
+  {
+    suit: "spade",
+    number: 1,
+  },
+  {
+    suit: "heart",
+    number: 1,
+  },
+  {
+    suit: "diamond",
+    number: 1,
+  },
+  {
+    suit: "club",
+    number: 1,
+  },
+  {
+    suit: "spade",
+    number: 13,
+  },
+  {
+    suit: "heart",
+    number: 13,
+  },
+  {
+    suit: "diamond",
+    number: 13,
+  },
+  {
+    suit: "club",
+    number: 13,
+  },
+  {
+    suit: "spade",
+    number: 12,
+  },
+  {
+    suit: "heart",
+    number: 12,
+  },
+  {
+    suit: "diamond",
+    number: 12,
+  },
+  {
+    suit: "club",
+    number: 12,
+  },
+  {
+    suit: "spade",
+    number: 11,
+  },
+  {
+    suit: "heart",
+    number: 11,
+  },
+  {
+    suit: "diamond",
+    number: 11,
+  },
+  {
+    suit: "club",
+    number: 11,
+  },
+  {
+    suit: "spade",
+    number: 10,
+  },
+  {
+    suit: "heart",
+    number: 10,
+  },
+  {
+    suit: "diamond",
+    number: 10,
+  },
+  {
+    suit: "club",
+    number: 10,
+  },
+  {
+    suit: "spade",
+    number: 9,
+  },
+  {
+    suit: "heart",
+    number: 9,
+  },
+  {
+    suit: "diamond",
+    number: 9,
+  },
+  {
+    suit: "club",
+    number: 9,
+  },
+  {
+    suit: "spade",
+    number: 8,
+  },
+  {
+    suit: "heart",
+    number: 8,
+  },
+  {
+    suit: "diamond",
+    number: 8,
+  },
+  {
+    suit: "club",
+    number: 8,
+  },
+  {
+    suit: "spade",
+    number: 7,
+  },
+  {
+    suit: "heart",
+    number: 7,
+  },
+  {
+    suit: "diamond",
+    number: 7,
+  },
+  {
+    suit: "club",
+    number: 7,
+  },
+  {
+    suit: "spade",
+    number: 6,
+  },
+  {
+    suit: "heart",
+    number: 6,
+  },
+  {
+    suit: "diamond",
+    number: 6,
+  },
+  {
+    suit: "club",
+    number: 6,
+  },
+  {
+    suit: "spade",
+    number: 5,
+  },
+  {
+    suit: "heart",
+    number: 5,
+  },
+  {
+    suit: "diamond",
+    number: 5,
+  },
+  {
+    suit: "club",
+    number: 5,
+  },
+  {
+    suit: "spade",
+    number: 4,
+  },
+  {
+    suit: "heart",
+    number: 4,
+  },
+  {
+    suit: "diamond",
+    number: 4,
+  },
+  {
+    suit: "club",
+    number: 4,
+  },
+  {
+    suit: "spade",
+    number: 3,
+  },
+  {
+    suit: "heart",
+    number: 3,
+  },
+  {
+    suit: "diamond",
+    number: 3,
+  },
+  {
+    suit: "club",
+    number: 3,
+  },
+  {
+    suit: "spade",
+    number: 2,
+  },
+  {
+    suit: "heart",
+    number: 2,
+  },
+  {
+    suit: "diamond",
+    number: 2,
+  },
+  {
+    suit: "club",
+    number: 2,
+  },
+]
---
+++ simu/validate-hand.js
@@ -0,0 +1,81 @@
+module.exports = (cards) => {
+  let will2Pair = false
+  let will3Card = false
+
+  // 2 Pair
+  const numHistory2Pair = []
+  let firstNumber = 0
+  for (const card of cards) {
+    numHistory2Pair.push(card.number)
+    if (2 === numHistory2Pair.filter(e => (e === card.number) && (firstNumber !== 0) && (firstNumber !== card.number)).length) {
+      will2Pair = true
+    }
+    if (2 === numHistory2Pair.filter(e => e === card.number).length) {
+      firstNumber = card.number
+    }
+  }
+
+
+  //4 Card
+  const numHistory4card = []
+  for (const card of cards) {
+    numHistory4card.push(card.number)
+    if (4 === numHistory4card.filter(e => e === card.number).length) 
+      return "4-card"
+  }
+
+  
+  //3 Card
+  const numHistory3Card = []
+  for (const card of cards) {
+    numHistory3Card.push(card.number)
+    if (3 === numHistory3Card.filter(e => e === card.number).length) 
+      will3Card = true
+  }
+  
+
+  //Full House
+  if (will2Pair && will3Card) {
+    return "full-house"
+  } else if (will2Pair) {
+    return "2-pair"
+  } else if (will3Card) {
+    return "3-card"
+  }
+
+  
+  // Straight
+  const nums = []
+  cards.forEach(card => nums.push(card.number ?? 0))
+  nums.sort(sortAsc)
+
+  if (JSON.stringify(nums) === JSON.stringify([1, 10, 11, 12, 13]))  // case: Broadway (A K Q J 10)
+    return "straight"
+
+  let countup = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0)
+      continue
+    if (nums[i] === nums[i - 1] + 1)
+      countup++
+  }
+  if (countup === 4)
+    return "straight"
+
+
+  // Flush
+  if (cards.every(e => e.suit === cards[0].suit))
+    return "flush"
+
+
+  // Straight Flush, Royal Straight Flush
+  ;  // nothing to do
+
+
+  // BUTA (+ 1 Pair)
+  return null
+}
+
+function sortAsc(a, b) {
+  return a - b
+}
